6d7c197b9104c404ec7e32847b1162217924de24
Aiden-Rodriguez <a.woodenrod@gmail.com>
2024-09-28 15:07:39 -0700

    catalogueCheck

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,13 +10,23 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
 
-    return [
-            {
-                "sku": "RED_POTION_0",
-                "name": "red potion",
-                "quantity": 1,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }
-        ]
+        row = result.mappings().one()  # Using mappings to access the columns by name
+
+        # Extract values from the row
+        num_green_potions = row['num_green_potions']
+        if num_green_potions > 0:
+
+            return [
+                {
+                    "sku": "GREEN_POTION_0",
+                    "name": "green potion",
+                    "quantity": num_green_potions,
+                    "price": 50,
+                    "potion_type": [0, 100, 0, 0],
+                }
+            ]
+        else :
+            return []
