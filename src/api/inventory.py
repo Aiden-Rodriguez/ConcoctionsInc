85702ea24b71684c6197f2329bfd964205d23cf9
Aiden-Rodriguez <a.woodenrod@gmail.com>
2024-09-30 20:14:18 -0700

    asgn2 first test

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -15,15 +15,23 @@ router = APIRouter(
 def get_inventory():
     """ """
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_ml, num_green_potions, gold FROM global_inventory"))
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml, num_green_potions, num_blue_ml, num_blue_potions, num_red_potions, num_red_ml, num_dark_potions, num_dark_ml, gold FROM global_inventory"))
         row = result.mappings().one()  # Using mappings to access the columns by name
 
         # Extract values from the row
         
         num_green_ml = row['num_green_ml']
         num_green_potions = row['num_green_potions']
+        num_blue_ml = row['num_blue_ml']
+        num_blue_potions = row['num_blue_potions']
+        num_red_ml = row['num_red_ml']
+        num_red_potions = row['num_red_potions']
+        num_dark_ml = row['num_dark_ml']
+        num_dark_potions = row['num_dark_potions']
+        num_potions = num_green_potions + num_blue_potions + num_dark_potions + num_red_potions
+        num_ml = num_red_ml + num_blue_ml + num_green_ml + num_dark_ml
         gold = row['gold']
-        return {"number_of_potions": num_green_potions, "ml_in_barrels": num_green_ml, "gold": gold}
+        return {"number_of_potions": num_potions, "ml_in_barrels": num_ml, "gold": gold}
 
 # Gets called once a day
 @router.post("/plan")
--- src/api/server.py
+++ src/api/server.py
@@ -8,11 +8,11 @@ import sys
 from starlette.middleware.cors import CORSMiddleware
 
 description = """
-Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+Concoctions Inc is the premier ecommerce site for all your alchemical desires.
 """
 
 app = FastAPI(
-    title="Central Coast Cauldrons",
+    title="Concoctions Inc",
     description=description,
     version="0.0.1",
     terms_of_service="http://example.com/terms/",
@@ -53,4 +53,4 @@ async def validation_exception_handler(request, exc):
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Central Coast Cauldrons."}
+    return {"message": "Welcome to Concoctions Inc.."}
