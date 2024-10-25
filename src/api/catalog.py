a5df79f33b121aee94b844821261b413a2703e5a
Aiden-Rodriguez <a.woodenrod@gmail.com>
2024-10-24 18:39:50 -0700

    catalog fix... AGAAIN FOR REAL?>

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -53,24 +53,19 @@ def get_catalog():
         potions_to_remove = []
         
         for potion_type in potion_in_inventory:
-            if day == "Edgeday":
-                if potion_type['potion_type'][0] == 0:
-                    catalogue_list.append(potion_type)
-                    potions_to_remove.append(potion_type)
-            elif day == "Arcanaday":
-                if potion_type['potion_type'][2] == 0:
-                    catalogue_list.append(potion_type)
-                    potions_to_remove.append(potion_type)
-            elif day == "Bloomday":
-                if potion_type['potion_type'][1] == 0:
-                    catalogue_list.append(potion_type)
-                    potions_to_remove.append(potion_type)
-
-            potion_in_inventory = [p for p in potion_in_inventory if p not in potions_to_remove]
-
-            #just start adding potions if no special day or if slots remain!
-            while len(catalogue_list) < 6 and len(potion_in_inventory) > 0:
-                potion_type = potion_in_inventory.pop(0)
+            if day == "Edgeday" and potion_type['potion_type'][0] != 100:
                 catalogue_list.append(potion_type)
+                potions_to_remove.append(potion_type)
+            elif day == "Arcanaday" and potion_type['potion_type'][2] != 100:
+                catalogue_list.append(potion_type)
+                potions_to_remove.append(potion_type)
+            elif day == "Bloomday" and potion_type['potion_type'][1] != 100:
+                catalogue_list.append(potion_type)
+                potions_to_remove.append(potion_type)
+
+        potion_in_inventory = [p for p in potion_in_inventory if p not in potions_to_remove]
+        while len(catalogue_list) < 6 and len(potion_in_inventory) > 0:
+            potion_type = potion_in_inventory.pop(0)
+            catalogue_list.append(potion_type)
 
         return catalogue_list
